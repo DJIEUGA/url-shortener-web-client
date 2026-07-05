13987741c30708023cf474d29414f8cc792f99df
Djieuga Djoubi <djieugadjoubirylenathan@gmail.com>
2026-07-05 18:32:03 +0100

    resolved merged conflict

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
->>>>>>> 3916b60ef6f6d0d543c411a68edcc986cfab7010
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-<<<<<<< HEAD
 );
-=======
-)
->>>>>>> 3916b60ef6f6d0d543c411a68edcc986cfab7010
